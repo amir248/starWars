025f00dd0bf442585ce2b47b11c96bc46270b46e
chikchicly <chikchicly@gmail.com>
2022-08-16 16:22:20 +0700

    oK

--- one.js
+++ one.js
@@ -2,7 +2,7 @@ console.log('oK');
 document.querySelector('body');
 const position={
   top:0,
-  left:0,
+  left:150,
   right: 0,
   bottom: 0
 }
@@ -10,39 +10,43 @@ window.addEventListener('keydown',(e)=>{
   // console.log(e.code);
   if(e.code=='ArrowUp'){
     console.log('ArrowUp');
-    position.bottom++;
+    position.bottom+=10;
     console.log(position.bottom);
     // document.querySelector('#user').style.left=`${position.left}`;
     document.querySelector('#user').style.cssText=`
+    left:${position.left}px;
     bottom:${position.bottom}px;
       display:gird;
       position:absolute;
     `;
   }else if(e.code=="ArrowDown"){
     console.log("Down");
-    position.bottom--;
+    position.bottom-=10;
     console.log(position.bottom);
     // document.querySelector('#user').style.left=`${position.left}`;
     document.querySelector('#user').style.cssText=`
+    left:${position.left}px;
     bottom:${position.bottom}px;
       display:gird;
       position:absolute;
     `;
   }else if(e.code=='ArrowLeft'){
-        position.left++;
+        position.left-=10;
         console.log(position.left);
         // document.querySelector('#user').style.left=`${position.left}`;
         document.querySelector('#user').style.cssText=`
+        bottom:${position.bottom}px;
         left:${position.left}px;
           display:gird;
           position:absolute;
         `;
     console.log('left');
   }else if(e.code=='ArrowRight'){
-    position.right++;
+    position.left+=10;
     console.log('Right');
     document.querySelector('#user').style.cssText=`
-    right:${position.right}px;
+    bottom:${position.bottom}px;
+    left:${position.left}px;
       display:gird;
       position:absolute;
     `;
